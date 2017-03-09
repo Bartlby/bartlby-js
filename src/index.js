3159ba09fc9b972c1b1b1b648c80f343135f0f10
Helmut Januschka <h.januschka@krone.at>
2017-03-09 10:40:30 +0100

    binify

--- src/index.js
+++ src/index.js
@@ -10,69 +10,14 @@ import Boss from "./boss"
 // Logging
 const logger = debug("bartlby")
 
-var config = {
-  worker: 3
+class Bartlby {
+  constructor(config) {
+    
+  }
+  start() {
+    logger("Bartlby Init1")
+    const boss = new Boss();
+    const sched = new Scheduler(boss);
+  }
 }
-
-
-logger("Bartlby Init")
-const boss = new Boss();
-const sched = new Scheduler(boss);
-
-
-process.on('SIGINT', function() {
-    logger("Caught interrupt signal");
-    process.exit();
-});
-
-
-/*
-
-
-
-
-
-const svc_obj = {
-    server: {
-        ip: "192.168.239.120",
-        port: 9030
-    },
-    check: {
-        plugin: "bartlby_load",
-        params: "-w 10 -c 10 -p",
-        timeout: 2000
-    }
-}
-
-
-var r = v1.check(svc_obj)
-    .then(result => {
-        logger("AGENT V1:")
-        logger(result);
-    })
-    .catch(({message}) => {
-        logger("Error: ", message)
-    });
-
-
-const svc_obj_agent_v2 = {
-    server: {
-        ip: "sheldon.krone.at",
-        port: 9030
-    },
-    check: {
-        plugin: "bartlby_load",
-        params: "-w 10 -c 10 -p",
-        timeout: 2000
-    }
-}
-
-var r = v2.check(svc_obj_agent_v2)
-    .then(result => {
-        logger("AGENT V2: SSL")
-        logger(result);
-    })
-    .catch(({message}) => {
-        logger("Error: ", message)
-    });
-    */
+export default Bartlby;
