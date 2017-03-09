ae0346720d6d9c22bb792a896ae35501d7d25321
Helmut Januschka <h.januschka@krone.at>
2017-03-09 11:10:06 +0100

    commander

--- src/bin/bartlby.js
+++ src/bin/bartlby.js
@@ -1,5 +1,21 @@
 #! /usr/bin/env node
+'use strict';
+import program from 'commander'
 import Bartlby from '../'
+
+program
+  .version('0.0.1')
+  .description('List files and folders')
+  .option('-a, --all [type]', 'Add the specified type of cheese [marble]', 'marble')
+  .option('-l, --long [type]','asdas', "def")
+  
+program.parse(process.argv);
+
+
+//console.log(program.all)
+//console.log(program.long)
+
+
 var bartlby = new Bartlby({});
 bartlby.start();
 
